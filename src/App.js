432f9f333b3a7af54b19e91bc6dcbbcbb3ad5029
Murilo Mendonça <murilomendonca@live.com>
2021-07-14 14:11:18 -0300

    connection api test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import { BASE_URL } from './assets/constants/BASE_URL'
 
 function App() {
-  const [user, setUser] = useState('Murilo')
+  const [user, setUser] = useState('')
+
+  function handleSearch() {
+    axios
+      .get(`${BASE_URL}/${user}/repos`)
+      .then((response) => console.log(response.data))
+  }
 
   return (
     <>
-      {user}
+      <p>{user}</p>
       <input
         className="userInput"
-        name="user"
-        id="user"
         type="text"
         placeholder="Usuário"
+        value={user}
+        onChange={(event) => setUser(event.target.value)}
       />
-      <button type="button">Pesquisar</button>
+      <button type="button" onClick={handleSearch}>
+        Pesquisar
+      </button>
     </>
   )
 }
